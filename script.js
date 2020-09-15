3afed112f68abcf1e1ff1c458e33e870c005dd9c
Mike Tkachuk <tkachuk2314@gmail.com>
2020-09-14 20:20:33 -0400

    Upload Test V 1.1

--- script.js
+++ script.js
@@ -94,3 +94,17 @@ info.innerHTML = slideShow[i].name;
 
 
 
+//Upload
+
+let upload ;
+let btnupload = document.getElementById("uploadbtn");
+
+btnupload.addEventListener("click", function(){
+    upload = document.getElementById("upload").value;
+    btnupload = document.getElementById("uploadbtn");
+    console.log(upload);
+
+    let acura = new Picture(upload, "Acura NSX.jpg");
+    slideShow.push(acura);
+});
+
